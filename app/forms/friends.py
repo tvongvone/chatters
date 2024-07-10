70a9aec8891783a63584e8872dc611559c8cf4f4
Tony Vongvone <tvongvone@gmail.com>
2024-07-09 22:36:57 -0700

    validators for friend form

--- app/forms/friends.py
+++ app/forms/friends.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField
+from wtforms import TextAreaField, StringField
 from wtforms.validators import DataRequired, Length
 
 class Friend(FlaskForm):
+    name = StringField('name', validators = [DataRequired(), Length(min = 1, max = 100)])
