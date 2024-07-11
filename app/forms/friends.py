69d2d780261b54b1dec69f9832e3fcc155b418ca
Tony Vongvone <tvongvone@gmail.com>
2024-07-10 23:17:39 -0700

    model validators

--- app/forms/friends.py
+++ app/forms/friends.py
@@ -4,3 +4,4 @@ from wtforms.validators import DataRequired, Length
 
 class Friend(FlaskForm):
     name = StringField('name', validators = [DataRequired(), Length(min = 1, max = 100)])
+    status = StringField('status' validators = [DataRequired(), Length(min = 0, max = 100)])
